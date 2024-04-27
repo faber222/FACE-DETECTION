41c665d370b6e20eb85e7eda8ee8635579aa4b93
faber222 <faberbernardo78@gmail.com>
2024-04-27 20:16:46 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+	VideoCapture video(0);
+	Mat img;
+	while(true){
+		video.read(img);
+		imshow("Frame", img);
+		waitKey(1);
+	}
+	return 0;
+}
